bd2182c997a59a0fdf7ec5d0388b1f4c2bde9fee
heartandkarma <heartandkarma@gmail.com>
2022-03-28 14:51:34 -0700

    got popular.jsx returning recipe titles and photos

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,22 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function Popular() {
+  const [popular, setPopular] = useState([]);
   useEffect(() => {
     getPopular();
   }, []);
 
-  const API_KEY = process.env.REACT_APP_API_KEY;
   function getPopular() {
     axios
       .get(
         `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10`
       )
       .then((res) => {
-        console.log(res.data);
+        setPopular(res.data.recipes);
       });
   }
-  return <div>Popular</div>;
+  return (
+    <div>
+      <h1>Popular</h1>
+      {popular.map((recipe) => {
+        return (
+          <div key={recipe.id}>
+            <h1>{recipe.title}</h1>
+            <img src={recipe.image} alt={recipe.title} />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Popular;
